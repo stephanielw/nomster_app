6c9fa320ca95f10b62aa242034b64324afefa7b0
stephanielw <stephanielw88@gmail.com>
2014-10-11 23:14:13 +0000

    Adding Pagination

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,5 +1,6 @@
 class PlacesController < ApplicationController
 	def index
-		@places=Place.all
+		#added some conidtion for where clause
+		@places=Place.all.paginate(:page => params[:page], :per_page => 2)
 	end
 end
