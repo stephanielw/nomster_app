3e25e189b6b73e287157be1b988d248109a8bb52
stephanielw <stephanielw88@gmail.com>
2014-10-15 06:57:12 +0000

    add more validation

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,4 +1,8 @@
 class Place < ActiveRecord::Base
 	belongs_to :user
-	validates :name, :presence => true
+	validates :name, :presence => true,:uniqueness => true, :length => { :minimum => 3 }
+	validates :address, :presence => true
+	validates :description, :presence => true
+
+
 end
