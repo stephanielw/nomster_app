4200f4957dac6a35b8aa3259cf591575f531c3c4
stephanielw <stephanielw88@gmail.com>
2014-10-25 05:20:29 +0000

    Adjusting the automated email mailer

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -3,7 +3,7 @@ class PlacesController < ApplicationController
 
 	def index
 		#added some conidtion for where clause
-		@places=Place.all.paginate(:page => params[:page], :per_page => 2)
+		@places=Place.all.paginate(:page => params[:page], :per_page => 4)
 	end
 
 	def new
